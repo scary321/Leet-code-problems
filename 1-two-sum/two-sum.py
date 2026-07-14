73a79e28436a7e2ffd07cb0f7ff9c317abfd5356
scary321 <139805188+scary321@users.noreply.github.com>
2026-07-14 17:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (53.14%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return i,j
-        
-        
+        n= len(nums)
+        freq_map ={}
+        for i in range(0,n):
+            remaining = target-nums[i]
+            if remaining in freq_map:
+                return [freq_map[remaining],i]
+            freq_map[nums[i]]=i
         
