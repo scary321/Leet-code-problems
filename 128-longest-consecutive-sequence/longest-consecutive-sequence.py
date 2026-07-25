242fd86078cadbfd6b9eecee9a2af0abc18292d8
scary321 <139805188+scary321@users.noreply.github.com>
2026-07-25 21:44:53 +0530

    Time: 84 ms (13.56%) | Memory: 27.4 MB (12.78%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -7,6 +7,7 @@ class Solution(object):
         longest=0
         for num in my_set:
             if num-1 not in my_set:
+                
                 x=num
                 count = 1
                 while x+1 in my_set:
